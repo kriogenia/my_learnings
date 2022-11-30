96172c7dbf4b836ac0e8355b3cbb646d0c0d19bd
Kriogenia <47500377+kriogenia@users.noreply.github.com>
2022-12-01 00:50:45 +0100

    Test entries CRUD operations

--- go_bank/db/sqlc/account_test.go
+++ go_bank/db/sqlc/account_test.go
@@ -21,11 +21,10 @@ func createRandomAccount(t *testing.T) Account {
 	require.NoError(t, err)
 	require.NotEmpty(t, account)
 
+	require.NotZero(t, account.ID)
 	require.Equal(t, arg.Owner, account.Owner)
 	require.Equal(t, arg.Balance, account.Balance)
 	require.Equal(t, arg.Currency, account.Currency)
-
-	require.NotZero(t, account.ID)
 	require.NotZero(t, account.CreatedAt)
 
 	return account
---
+++ go_bank/db/sqlc/entry_test.go
@@ -0,0 +1,69 @@
+package db
+
+import (
+	"context"
+	"testing"
+	"time"
+
+	"github.com/kriogenia/my_learnings/go_bank/util"
+	"github.com/stretchr/testify/require"
+)
+
+func createRandomEntry(t *testing.T, account Account) Entry {
+	args := CreateEntryParams{
+		AccountID: account.ID,
+		Amount:    util.RandomMoney(),
+	}
+
+	entry, err := testQueries.CreateEntry(context.Background(), args)
+	require.NoError(t, err)
+	require.NotEmpty(t, entry)
+
+	require.NotZero(t, entry.ID)
+	require.Equal(t, account.ID, entry.AccountID)
+	require.Equal(t, args.Amount, entry.Amount)
+	require.NotZero(t, entry.CreatedAt)
+
+	return entry
+}
+
+func TestCreateEntry(t *testing.T) {
+	account := createRandomAccount(t)
+	createRandomEntry(t, account)
+}
+
+func TestGetEntry(t *testing.T) {
+	account := createRandomAccount(t)
+	created := createRandomEntry(t, account)
+
+	retrieved, err := testQueries.GetEntry(context.Background(), created.ID)
+	require.NoError(t, err)
+	require.NotEmpty(t, retrieved)
+
+	require.Equal(t, created.ID, retrieved.ID)
+	require.Equal(t, created.Amount, retrieved.Amount)
+	require.Equal(t, created.AccountID, retrieved.AccountID)
+	require.WithinDuration(t, created.CreatedAt, retrieved.CreatedAt, time.Second)
+}
+
+func TestListEntries(t *testing.T) {
+	account := createRandomAccount(t)
+	for i := 0; i < 10; i++ {
+		createRandomEntry(t, account)
+	}
+
+	args := ListEntriesParams{
+		AccountID: account.ID,
+		Limit:     5,
+		Offset:    5,
+	}
+
+	entries, err := testQueries.ListEntries(context.Background(), args)
+	require.NoError(t, err)
+	require.Len(t, entries, 5)
+
+	for _, entry := range entries {
+		require.NotEmpty(t, entry)
+		require.Equal(t, account.ID, entry.AccountID)
+	}
+}
