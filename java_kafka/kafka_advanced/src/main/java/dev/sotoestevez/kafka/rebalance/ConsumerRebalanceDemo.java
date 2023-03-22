ad5d93d36836dcbe80c0b09b04ea69fcdbb5ecc9
Kriogenia <47500377+kriogenia@users.noreply.github.com>
2023-03-22 23:16:43 +0100

    Rebalance Demo

--- java_kafka/kafka_advanced/src/main/java/dev/sotoestevez/kafka/rebalance/ConsumerRebalanceDemo.java
+++ java_kafka/kafka_advanced/src/main/java/dev/sotoestevez/kafka/rebalance/ConsumerRebalanceDemo.java
@@ -1,4 +1,4 @@
-package dev.sotoestevez.events;
+package dev.sotoestevez.kafka.rebalance;
 
 import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.clients.consumer.KafkaConsumer;
@@ -11,16 +11,16 @@ import java.time.Duration;
 import java.util.Collections;
 import java.util.Properties;
 
-public class ConsumerDemo {
+public class ConsumerRebalanceDemo {
 
-    private static final Logger log = LoggerFactory.getLogger(ConsumerDemo.class.getSimpleName());
+    private static final Logger log = LoggerFactory.getLogger(ConsumerRebalanceDemo.class);
 
-    private static final String GROUP_ID = "java_kafka_consumer";
+    private static final String GROUP_ID = "java_kafka_rebalance_consumer";
+    private static final String TOPIC = "java_kafka";
 
     public static void main(String[] args) {
-        log.info("Hello consumer!");
+        log.info("Consumer Rebalance Demo");
 
-        // create properties
         var props = new Properties();
         props.setProperty(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "127.0.0.1:9092");
         props.setProperty(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
@@ -28,44 +28,55 @@ public class ConsumerDemo {
         props.setProperty(ConsumerConfig.GROUP_ID_CONFIG, GROUP_ID);
         props.setProperty(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
 
-        // create a consumer
-        var consumer = new KafkaConsumer<String, String>(props);
+        // autocommit disabled to manually manage it
+        props.setProperty(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, "false");
 
-        // get a reference to the main thread
-        final var mainThread = Thread.currentThread();
-        // and a shutdown hook
+        var consumer = new KafkaConsumer<String, String>(props);
+        var listener = new MyConsumerRebalanceListener(consumer);
+
+        final Thread mainThread = Thread.currentThread();
+
+        // shutdown hook to gracefully close the consumer
         Runtime.getRuntime().addShutdownHook(new Thread(() -> {
-            log.info("Shutdown detected, exiting gracefully...");
+            log.info("Detected a shutdown. Exiting consumer gracefully");
             consumer.wakeup();
 
-            // join the main thread to execute the code
             try {
                 mainThread.join();
-            } catch (InterruptedException e) {
-                throw new RuntimeException(e);
+            } catch (InterruptedException ie) {
+                log.error("Unable to join main thread", ie);
             }
         }));
 
         try {
-            // subscribe to a topic
-            consumer.subscribe(Collections.singletonList(ProducerDemo.TOPIC));
+            // subscribe to a topic, specifying the listener
+            consumer.subscribe(Collections.singletonList(TOPIC), listener);
 
-            // poll for data
             while (true) {
-                log.info("Polling");
                 var records = consumer.poll(Duration.ofMillis(1000));
                 for (var record: records) {
                     log.info("KEY: " + record.key() + ", VALUE: " + record.value());
                     log.info("PARTITION: " + record.partition() + ", OFFSET: " + record.offset());
+
+                    // keep track the offset
+                    listener.addOffsetToTrack(record.topic(), record.partition(), record.offset());
                 }
+                // commit async as all data has been consumed and there's no intention to block the thread
+                consumer.commitAsync();
             }
         } catch (WakeupException wue) {
             log.info("Consumer is starting to shutdown");
         } catch (Exception e) {
             log.error("Unexpected exception in the consumer", e);
         } finally {
-            consumer.close();
-            log.info("Consumer closed");
+            try {
+                // commit the offset before closing the consumer
+                consumer.commitSync(listener.getCurrentOffsets());
+            } finally {
+                consumer.close();
+                log.info("The consumer has been gracefully closed and the offsets committed");
+            }
         }
+
     }
 }
