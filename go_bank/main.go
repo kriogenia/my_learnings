a11f1b82bcc1471ca469993203e2dd38665416fd
Kriogenia <47500377+kriogenia@users.noreply.github.com>
2023-04-21 11:44:41 +0200

    Implement gRPC server

--- go_bank/main.go
+++ go_bank/main.go
@@ -3,11 +3,16 @@ package main
 import (
 	"database/sql"
 	"log"
+	"net"
 
 	"github.com/kriogenia/my_learnings/go_bank/api"
 	db "github.com/kriogenia/my_learnings/go_bank/db/sqlc"
+	"github.com/kriogenia/my_learnings/go_bank/gapi"
+	"github.com/kriogenia/my_learnings/go_bank/pb"
 	"github.com/kriogenia/my_learnings/go_bank/util"
 	_ "github.com/lib/pq"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
 )
 
 func main() {
@@ -22,13 +27,40 @@ func main() {
 	}
 
 	store := db.NewStore(conn)
+	//runGinServer(config, store)
+	runGrpcServer(config, store)
+}
+
+func runGinServer(config util.Config, store db.Store) {
 	server, err := api.NewServer(config, store)
 	if err != nil {
 		log.Fatal("unable to create server: ", err)
 	}
 
-	err = server.Start(config.ServerAddress)
+	err = server.Start(config.HTTPServerAddress)
 	if err != nil {
 		log.Fatal("unable to start server: ", err)
+	}
+}
+
+func runGrpcServer(config util.Config, store db.Store) {
+	server, err := gapi.NewServer(config, store)
+	if err != nil {
+		log.Fatal("unable to create server: ", err)
+	}
+
+	grpcServer := grpc.NewServer()
+	pb.RegisterGoBankServer(grpcServer, server)
+	reflection.Register(grpcServer)
+
+	listener, err := net.Listen("tcp", config.GRPCServerAddress)
+	if err != nil {
+		log.Fatal("unable to create listener")
+	}
+
+	log.Printf("starting gRPC server at %s", listener.Addr().String())
+	err = grpcServer.Serve(listener)
+	if err != nil {
+		log.Fatal("unable to start gRPC server")
 	}
 }
