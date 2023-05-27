9142e8d3b5271a99f28f8d24357dd15268ef9460
Kriogenia <47500377+kriogenia@users.noreply.github.com>
2023-05-27 16:25:59 +0200

    Add AskPattern based distributed word count example

--- scala_akka_essentials/src/main/scala/examples/DistributedWordCountActor.scala
+++ scala_akka_essentials/src/main/scala/examples/DistributedWordCountActor.scala
@@ -1,8 +1,29 @@
 package examples
 
 import akka.actor.{Actor, ActorRef, ActorSystem, Props}
+import akka.pattern.{ask, pipe}
+import akka.util.Timeout
+import org.scalatest.time.SpanSugar.convertIntToGrainOfTime
+
+import scala.concurrent.ExecutionContext
+import scala.language.postfixOps
+import scala.util.Success
 
 object DistributedWordCountActor extends App {
+
+
+  private object WordCounterWorker {
+    case class Task(id: Int, text: String)
+  }
+
+  private class WordCounterWorker extends Actor {
+
+    import WordCounterWorker._
+
+    override def receive: Receive = {
+      case Task(id, text) => sender() ! WordCounterMaster.WordCountReply(id, text.split(" ").length)
+    }
+  }
 
   private object WordCounterMaster {
     case class Initialize(numChildren: Int)
@@ -33,16 +54,27 @@ object DistributedWordCountActor extends App {
         sender ! value
         context.become(initialized(childs, State(state.index, computed, state.nextId)))
     }
-
   }
 
-  private object  WordCounterWorker {
-    case class Task(id: Int, text: String)
-  }
-  private class WordCounterWorker extends Actor {
+  private class WordCounterMasterAskPattern extends Actor {
+    import WordCounterMaster._
     import WordCounterWorker._
+
+    implicit val timeout: Timeout = Timeout(1 second)
+    implicit val executionContext: ExecutionContext = context.dispatcher
+
     override def receive: Receive = {
-      case Task(id, text) => sender() ! WordCounterMaster.WordCountReply(id, text.split(" ").length)
+      case Initialize(numChildren) =>
+        val childs = (0 to numChildren).map(
+          n => context.system.actorOf(Props[WordCounterWorker], s"ask_worker-$n")
+        ).toList
+        context.become(initialized(childs, 0))
+    }
+
+    private def initialized(childs: List[ActorRef], index: Int): Receive = {
+      case text: String =>
+        (childs(index) ? Task(0, text)).mapTo[WordCountReply].map(r => r.count).pipeTo(sender())
+        context.become(initialized(childs, (index + 1) % childs.length))
     }
   }
 
@@ -67,5 +99,18 @@ object DistributedWordCountActor extends App {
   o4 ! "what a weird fox"
   o3 ! "he went into mexico"
   o2 ! "jumped over the wall"
+
+  private val askMaster = system.actorOf(Props[WordCounterMasterAskPattern], "ask")
+  askMaster ! WordCounterMaster.Initialize(3)
+
+  private val a1 = system.actorOf(Props(new OrchestratorActor(askMaster)), "a1")
+  private val a2 = system.actorOf(Props(new OrchestratorActor(askMaster)), "a2")
+  private val a3 = system.actorOf(Props(new OrchestratorActor(askMaster)), "a3")
+  private val a4 = system.actorOf(Props(new OrchestratorActor(askMaster)), "a4")
+
+  a1 ! "a brown fox"
+  a4 ! "what a weird fox"
+  a3 ! "he went into mexico"
+  a2 ! "jumped over the wall"
 
 }
