c1f72eaec94728bbf1baf3493baf73a93941114a
Kriogenia <47500377+kriogenia@users.noreply.github.com>
2023-10-19 14:36:58 +0100

    Add exception handling example

--- scala_akka/http/src/main/scala/highlevel/ExceptionHandling.scala
+++ scala_akka/http/src/main/scala/highlevel/ExceptionHandling.scala
@@ -1,5 +1,31 @@
 package highlevel
 
-object ExceptionHandling {
+import akka.http.scaladsl.Http
+import akka.http.scaladsl.model.StatusCodes
+import akka.http.scaladsl.server.Directives._
+import akka.http.scaladsl.server.ExceptionHandler
+import common.HttpApp
+
+object ExceptionHandling extends HttpApp {
+
+  private val simpleRoute = path("api") {
+    get {
+      throw new RuntimeException("runtime exception")
+    } ~
+    post {
+      parameter(Symbol("id")) { id =>
+        if (id.length > 1) throw new IllegalArgumentException("wrong id") else complete(StatusCodes.OK)
+      }
+    }
+  }
+
+  private val customExceptionHandler: ExceptionHandler = ExceptionHandler {
+    case e: RuntimeException => complete(StatusCodes.InternalServerError, e.getMessage)
+    case e: IllegalArgumentException => complete(StatusCodes.BadRequest, e.getMessage)
+  }
+
+  Http().newServerAt(host, defaultPort).bind(handleExceptions(customExceptionHandler) {
+    simpleRoute
+  })
 
 }
