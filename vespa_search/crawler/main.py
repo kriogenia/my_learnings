fdd4c8994f5ae4f3a35ed15311e6f1b192595c43
Kriogenia <47500377+kriogenia@users.noreply.github.com>
2024-09-09 18:14:54 +0200

    feat: query movie details

--- vespa_search/crawler/main.py
+++ vespa_search/crawler/main.py
@@ -63,8 +63,16 @@ def fetch_ids(n: int):
         if is_limit_reached(i):
             break
         ids.append(line[20:].split(",", 1)[0])
-    print(f"Fetched the IDs of {len(ids)} movies")
+    print(f"> Fetched the IDs of {len(ids)} movies")
     return ids
+
+
+def query_movie(movie_id: str, token: str):
+    response = request_tmdb(f"/movie/{movie_id}?language=en-US", token)	# todo: allow language selection?
+    if response.status_code != 200:
+        print(f"Failed to get the details of the movie [{movie_id}]")
+        return
+    return response.text
 
 
 def print_status():
@@ -79,4 +87,10 @@ if __name__ == "__main__":
     args = parse_args()
     authenticate(args.token)
     movie_ids = fetch_ids(args.number)
+    # todo: move to function
+    for movie_id in movie_ids:
+        movie = query_movie(movie_id, args.token)
+        print(movie)
+        # todo: add wait
+        counter += 1
     threading._start_new_thread(print_status, ())
